7f2b1e66376f191be25c53c528b300a198ec69d4
renatolazo <54364273+renatolazo@users.noreply.github.com>
2019-11-07 23:24:14 -0500

    Update Persona.h

--- TrabajoF/TrabajoFinal/Persona.h
+++ TrabajoF/TrabajoFinal/Persona.h
@@ -1,16 +1,19 @@
+#include <iostream>
 #ifndef PERSONA_H
 #define PERSONA_H
 
+using namespace std;
 
 class Persona
 {
-    public:
-        Persona();
-        virtual ~Persona();
-
-    protected:
-
-    private:
+private:
+    int edad;
+    string nombre;
+public:
+    Persona(int , string);
+    int ObtenerEdad();
+    void leer();
+    void obtener();
 };
 
 #endif // PERSONA_H
