6fe8caf8f47d3091de13f43f7ee1dc4707f14713
renatolazo <54364273+renatolazo@users.noreply.github.com>
2019-11-27 01:51:56 -0500

    Update LibInformativos.cpp

--- TrabajoF/TrabajoFinal/src/LibInformativos.cpp
+++ TrabajoF/TrabajoFinal/src/LibInformativos.cpp
@@ -4,12 +4,19 @@
 
 using namespace std;
 
-LibInformativos::LibInformativos(int _fecha, string _autor, string _genero, string _editorial, string _titulo) : Libro(_fecha,_autor,_genero,_editorial)
+LibInformativos::LibInformativos(int _fecha, int _NumOpc, string _autor, string _titulo2, string _materia) : Libro(_fecha, _NumOpc, _autor)
 {
-    titulo = _titulo;
+    titulo2 = _titulo2;
+    materia = _materia;
 }
 
-void LibInformativos::sobre()
+void LibInformativos::getSobre()
 {
-    cout << "la implementacion de la trigonometria en la matematica" << endl;
+    Libro::getSobre();
+    cout << "\n   3. Materia: " << materia << endl;
+}
+
+void LibInformativos::getTitulo2()
+{
+    cout << titulo2 << endl;
 }
